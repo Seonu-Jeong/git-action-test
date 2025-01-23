8a3f85d0ecea0ffc24ab21b17b425de16fa21806
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-23 21:21:35 +0900

    init project

---
+++ src/test/java/org/sparta/ci_test/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.ci_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
